0dd3bff9f8ec79fdca014a20caca240b9f20143b
JeongSeonggil <dataofsg02@gmail.com>
2022-07-25 20:44:04 +0900

    JetBrains Academy : Task List (2/6)

--- src/main/kotlin/tasklist/Main.kt
+++ src/main/kotlin/tasklist/Main.kt
@@ -1,26 +1,35 @@
 package tasklist
 
 fun main() {
-    println("Input an action (add, print, end):")
-    val mutList = emptyList<String>().toMutableList()
-    var run = true
+    mutableListOf<MutableList<String>>().action()
+}
 
-    while (run) {
-        val line = readln().trim()
-        if (line.isEmpty()) { //
-            run = false // 실행 종료
-            break
-        } else mutList.add(line) // 작업 추가
+fun MutableList<MutableList<String>>.action() {
+    when (println("Input an action (add, print, end):").run { readln() }) {
+        "add" -> addTask()
+        "print" -> printTasks()
+        "end" -> println("Tasklist exiting!").run { return }
+        else -> println("The input action is invalid")
     }
-    // 등록된 작업이 없을 경우
-    if (mutList.isEmpty()) println("No tasks have been input") else { //  있다면 작업 출력
-        for (index in mutList.indices) {
-            val spacing = if (index < 9) "  " else " "
-            println("${index + 1}$spacing${mutList[index]}")
-        }
+    action()
+}
+
+fun MutableList<MutableList<String>>.addTask() {
+    println("Input a new task (enter a blank line to end):")
+    with(mutableListOf<String>()) {
+        addToDoList().also { if (isNotEmpty()) add(this) else println("The task is blank") }
     }
+}
 
+fun MutableList<String>.addToDoList() {
+    readln().trimIndent().let { if (it.isNotEmpty()) this.add(it).also { addToDoList() } }
+}
 
+fun MutableList<MutableList<String>>.printTasks() {
+    if (isEmpty()) println("No tasks have been input")
+    mapIndexed { index, ToDoList -> println(
+        "${(index + 1).let { if (it < 10) "$it  " else "$it " }}${ToDoList.joinToString("\n   ")}\n"
+    ) }
 }
 
 
